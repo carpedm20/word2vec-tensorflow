936657c23bcf90aad058df4f3256d1f7572865fb
Taehoon Kim <carpedm20@gmail.com>
2015-12-09 23:02:06 +0900

    possible to run main.py

--- main.py
+++ main.py
@@ -5,23 +5,24 @@ from model import Word2Vec
 config = {}
 config['corpus'] = "corpus.txt" # input data
 config['window'] = 5 # (maximum) window size
-config['dim'] = 100 # dimensionality of word embeddings
+config['embed_size'] = 100 # dimensionality of word embeddings
 config['alpha'] = 0.75 # smooth out unigram frequencies
-config['table_size'] = 1e8 # table size from which to sample neg samples
-config['neg_samples'] = 5 # number of negative samples for each positive sample
-config['minfreq'] = 10 #threshold for vocab frequency
+config['table_size'] = int(1E8) # table size from which to sample neg samples
+config['neg_sample_size'] = 5 # number of negative samples for each positive sample
+config['min_frequency'] = 10 #threshold for vocab frequency
 config['lr'] = 0.025 # initial learning rate
 config['min_lr'] = 0.001 # min learning rate
 config['epochs'] = 3 # number of epochs to train
 config['gpu'] = 0 # 1 = use gpu, 0 = use cpu
 config['stream'] = 1 # 1 = stream from hard drive 0 = copy to memory first
 
-w2v = Word2Vec(config)
-w2v.build_vocab(config.corpus)
-w2v.build_table()
+with tf.Session() as sess:
+    w2v = Word2Vec(config, sess)
+    w2v.build_vocab(config['corpus'])
+    w2v.build_table()
 
-for idx in xrange(config['epochs']):
-    w2v.lr = config['lr']
-    w2v.train_model(config['corpus'])
+    for idx in xrange(config['epochs']):
+        w2v.lr = config['lr']
+        w2v.train_model(config['corpus'])
 
-w2v.print_sim_words(['the', 'he', 'can'], 5)
+    w2v.print_sim_words(['the', 'he', 'can'], 5)
--- model.py
+++ model.py
@@ -4,29 +4,30 @@ from __future__ import print_function
 
 import os
 import time
+import math
 import random
+import numpy as np
 import tensorflow as tf
-from collections import defaultdict, Counter
+from collections import Counter
 
 class Word2Vec(object):
-    def __init__(self, config):
-        self.gpu = config.gpu
-        self.alpha = config.alpha
-        self.stream = config.stream
-        self.neg_sample_size = config.neg_sample_size
-        self.min_frequency = config.min_frequency
-        self.window = config.window
-        self.lr = config.lr
-        self.min_lr = config.min_lr
-        self.table_size = config.table_size # unigram table size
+    def __init__(self, config, sess):
+        self.alpha = config['alpha']
+        self.embed_size = config['embed_size']
+        self.neg_sample_size = config['neg_sample_size']
+        self.min_frequency = config['min_frequency']
+        self.window = config['window']
+        self.lr = config['lr']
+        self.min_lr = config['min_lr']
+        self.table_size = config['table_size'] # unigram table size
 
     def build_vocab(self, filename):
         start_time = time.time()
-        with open(filename) as f
+        with open(filename) as f:
             words = [word for line in f.readlines() for word in line.split()]
         self.total_count = len(words)
         self.counter = []
-        self.counter.extend([item for item in collections.Counter(words).most_common()
+        self.counter.extend([list(item) for item in Counter(words).most_common()
                           if item[0] > self.min_frequency])
         self.vocab_size = len(self.counter)
         word2idx = dict()
@@ -41,16 +42,37 @@ class Word2Vec(object):
                 index = 0 # word2idx['UNK']
                 unk_count = unk_count + 1
             data.append(index)
-        self.count[0][1] = unk_count
+        self.counter[0][1] = unk_count
         idx2word = dict(zip(word2idx.values(), word2idx.keys()))
         duration = time.time() - start_time
-        print("%d words processed in %.2f seconds", % (self.total_count, duration))
-        print("Vocab size after eliminating words occuring less than %d times: %d", % (self.min_frequency, self.vocab_size))
+
+        print("%d words processed in %.2f seconds" % (self.total_count, duration))
+        print("Vocab size after eliminating words occuring less than %d times: %d" % (self.min_frequency, self.vocab_size))
+
         self.data = data
         self.words = words
         self.word2idx = word2idx 
         self.idx2word = idx2word
+
         self.decay = (self.min_lr-self.lr)/(self.total_count*self.window)
+        self.labels = np.zeros(1+self.neg_sample_size, dtype=np.float32); self.labels[0] = 1
+        self.build_model()
+
+    def build_model(self):
+        self.x = tf.placeholder(tf.int32, [1], name='pos_x')
+
+        init_width = 0.5 / self.embed_size
+        self.embed = tf.Variable(tf.random_uniform([self.vocab_size, self.embed_size], -init_width, init_width), name='embed')
+        self.w = tf.Variable(tf.truncated_normal([self.vocab_size, self.embed_size], stddev=1.0 / math.sqrt(self.embed_size)), name='w')
+
+        self.x_embed = tf.nn.embedding_lookup(self.embed, self.x, name='pos_embed')
+        self.x_w = tf.nn.embedding_lookup(self.w, self.x, name='pos_embed')
+
+        self.mul = tf.matmul(self.x_embed, self.x_w, transpose_b=True)
+        self.p = tf.nn.sigmoid(self.mul)
+
+        self.loss = tf.nn.sigmoid_cross_entropy_with_logits(self.p, self.labels)
+        self.train = tf.train.GradientDescentOptimizer(0.001).minimize(self.loss)
 
     def build_table(self):
         start_time = time.time()
@@ -59,12 +81,12 @@ class Word2Vec(object):
             total_count_pow += math.pow(count, self.alpha)
         word_index = 0
         self.table = np.ndarray([self.table_size])
-        word_prob = math.pow(self.counter[word_index], self.alph) / total_count_pow
+        word_prob = math.pow(self.counter[word_index][1], self.alpha) / total_count_pow
         for idx in xrange(self.table_size):
             self.table[idx] = word_index
             if idx / self.table_size > word_prob:
                 word_index += 1
-                word_prob += math.pow(self.counter[word_index], self.alph) / total_count_pow
+                word_prob += math.pow(self.counter[word_index][1], self.alpha) / total_count_pow
             if word_index > self.vocab_size:
                 word_index = word_index - 1
         print(string.format("Done in %.2f seconds.", time.time() - start))
@@ -87,5 +109,38 @@ class Word2Vec(object):
         start_time = time.time()
         c = 0
         with open(filename) as f:
-            sentence = f.readlines()
+            words = [word for line in f.readlines() for word in line.split()]
+            for idx, word in enumerate(words):
+                try:
+                    word_idx = self.word2idx[word]
+                    reduced_window = random.rand(self.window)
+                    self.words[0] = word_idx
+                    for jdx in xrange(idx - reduced_window, idx + reduced_window + 1):
+                        context = words[jdx]
+                        if j != idx:
+                            try:
+                                context_idx = self.word2idx[context]
+                                self.sample_contexts(context_id)
+                                self.train_pair(word_idx, self.contexts)
+                                self.lr = math.max(self.min_lr, self.lr + self.decay)
+                                c += 1
+                                if c % 10000 == 0:
+                                    print("%d words trained in %.2f seconds. Learning rate: %.4f" % (c, time.time() - time, self.lr))
+                            except:
+                                continue
+                except:
+                    continue
+
+    def get_sim_Words(self, idxs, k):
+        idxs = np.array(idxs)
+        vals, idxs = sess.run(
+            [nearby_val, nearby_idx], {nearby_character: idxs})
+        for i in xrange(len(idxs)):
+            print(idx2word[idxs[i]])
+            print()
+            for (neighbor, distance) in zip(idx[i, :num], vals[i, :num]):
+                print("%-20s %6.4f" % (idx2word[neighbor], distance))
+
+    def train_model(corpus):
+        self.train_stream(corpus)
 
